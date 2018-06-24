5b7b013a17c002d0aa831c03c060e723dc1834fa
deepgautam <deepgautam2008@gmail.com>
2018-06-24 17:10:10 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,91 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {Chart} from 'chart.js'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'DotNet Techy, Prsenting the ChartJs With Angular 6';
+  LineChart=[];
+  BarChart=[];
+
+  ngOnInit()
+  {
+    // Line chart:
+this.LineChart = new Chart('lineChart', {
+  type: 'line',
+data: {
+ labels: ["Jan", "Feb", "March", "April", "May", "June","July","Aug","Sep","Oct","Nov","Dec"],
+ datasets: [{
+     label: 'Number of Items Sold in Months',
+     data: [9,7 , 3, 5, 2, 10,15,16,19,3,1,9],
+     fill:false,
+     lineTension:0.2,
+     borderColor:"red",
+     borderWidth: 1
+ }]
+}, 
+options: {
+ title:{
+     text:"Line Chart",
+     display:true
+ },
+ scales: {
+     yAxes: [{
+         ticks: {
+             beginAtZero:true
+         }
+     }]
+ }
+}
+});
+
+// Bar chart:
+this.BarChart = new Chart('barChart', {
+  type: 'bar',
+data: {
+ labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+ datasets: [{
+     label: '# of Votes',
+     data: [9,7 , 3, 5, 2, 10],
+     backgroundColor: [
+         'rgba(255, 99, 132, 0.2)',
+         'rgba(54, 162, 235, 0.2)',
+         'rgba(255, 206, 86, 0.2)',
+         'rgba(75, 192, 192, 0.2)',
+         'rgba(153, 102, 255, 0.2)',
+         'rgba(255, 159, 64, 0.2)'
+     ],
+     borderColor: [
+         'rgba(255,99,132,1)',
+         'rgba(54, 162, 235, 1)',
+         'rgba(255, 206, 86, 1)',
+         'rgba(75, 192, 192, 1)',
+         'rgba(153, 102, 255, 1)',
+         'rgba(255, 159, 64, 1)'
+     ],
+     borderWidth: 1
+ }]
+}, 
+options: {
+ title:{
+     text:"Bar Chart",
+     display:true
+ },
+ scales: {
+     yAxes: [{
+         ticks: {
+             beginAtZero:true
+         }
+     }]
+ }
+}
+});
+
+
+
+
+  }
 }
